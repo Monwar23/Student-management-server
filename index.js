92ac8569fc90b2d9a50094424a4259accb186e8f
monwar23 <monwar2345@gmail.com>
2024-07-09 02:21:42 +0600

    complete task

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const corsOptions = {
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
+      'https://student-management-server-sigma.vercel.app',
+      'https://student-management-23.vercel.app'
   
     ],
     credentials: true,
